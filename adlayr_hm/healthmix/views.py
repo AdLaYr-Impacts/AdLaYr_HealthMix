b8c5b4cd7125ee3fd077112ca74afa9d0cd0d3a7
AdLaYr-Impacts <adlayrimpacts@gmail.com>
2025-10-25 12:52:11 +0530

    banner image update

--- adlayr_hm/healthmix/views.py
+++ adlayr_hm/healthmix/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from healthmix.models import (
+    BannerImage
+)
 
 class HomeView(View):
     def get(self,request,*args,**kwargs):
-        return render(request, 'adlayr_hm/home.html')
+        banner_image = BannerImage.objects.filter(is_active=True).first()
+        data = {
+            "banner_image": banner_image.image.url
+        }
+        return render(request, 'adlayr_hm/home.html', context=data)

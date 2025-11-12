2fbe281d8f0e2d598d86dee6fb1d58e08bcf4f95
AdLaYr-Impacts <adlayrimpacts@gmail.com>
2025-11-12 23:22:01 +0530

    implement user registeration configs

---
+++ adlayr_hm/authentication/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from .models import Profile
+
+class RegisterForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'id': 'confirm_password',
+            'class': 'form-control form-input',
+            'required': 'required',
+            'placeholder': 'Confirm password'
+        }),
+        label="Confirm Password"
+    )
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'email', 'password']
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control form-input',
+                'required': 'required', 
+                'placeholder': 'Enter your name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control form-input',
+                'required': 'required',
+                'placeholder': "example@gmail.com"
+            }),
+            'password': forms.PasswordInput(attrs={
+                'id': 'password',
+                'class': 'form-control form-input',
+                'required': 'required',
+                'placeholder': 'Enter password'
+            })
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords didn't match")
+        return cleaned_data
+    
+    def save(self, commit = True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- adlayr_hm/authentication/views.py
+++ adlayr_hm/authentication/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from healthmix.models import (
     BannerImage,
 )
+from .forms import RegisterForm
 
 class SignUpViewset(View):
+    form_class = RegisterForm
     def get(self,request,*args,**kwargs):
+        banner_image = BannerImage.objects.filter(is_active=True).first()
+        form = self.form_class()
+        data = {
+            "banner_image": banner_image.image.url,
+            "form": form,
+        }
+        return render(request, 'authentication/signup.html', context=data)
+
+    def post(self,request,*args,**kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
         banner_image = BannerImage.objects.filter(is_active=True).first()
         data = {
             "banner_image": banner_image.image.url,
+            'form': form
         }
         return render(request, 'authentication/signup.html', context=data)
 
